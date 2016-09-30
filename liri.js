2e30c584ac78e946dbbb2938930b7b562e9afec8
MTibby92 <MTibby92@gmail.com>
2016-09-29 20:32:57 -0500

    begin work on handling commands

--- liri.js
+++ liri.js
@@ -1,6 +1,8 @@
 var twitterObj = require('./keys.js')
 var twitterKeys = twitterObj.twitterKeys
 
+var spotify = require('spotify')
+
 // var consumer_key = twitterKeys.consumer_key
 // var consumer_secret = twitterKeys.consumer_secret
 // var access_token_key = twitterKeys.access_token_key
@@ -8,3 +10,29 @@ var twitterKeys = twitterObj.twitterKeys
 
 console.log(twitterKeys.consumer_key)
 console.log(consumer_key)
+
+var command = process.argv[2]
+var userData = process.argv[3]
+var defaultSong = 'The Sign'
+var defaultMovie = 'Mr. Nobody'
+
+switch(command) {
+	case 'my-tweets':
+		// do something
+	case 'spotify-this-song':
+		if (userData === undefined) {
+			spotify.search({type: 'track', query: defaultSong}, function (err,data) {
+				if (err) {throw err}
+
+			})
+		} else {
+			spotify.search({type: 'track', query: userData}, function (err,data) {
+				if (err) {throw err}
+
+			})
+		}
+	case 'movie-this':
+		// do something different
+	case 'do-what-it-says':
+		// yet another thing 
+}
