3b03ea3ffcf464a9b253c5c941681138e8d6c7c9
MTibby92 <MTibby92@gmail.com>
2016-09-30 10:08:59 -0500

    movie-this function is now working fully

--- liri.js
+++ liri.js
@@ -2,13 +2,14 @@ var twitterObj = require('./keys.js')
 var twitterKeys = twitterObj.twitterKeys
 
 var spotify = require('spotify')
+var request = require('request')
 
 // var consumer_key = twitterKeys.consumer_key
 // var consumer_secret = twitterKeys.consumer_secret
 // var access_token_key = twitterKeys.access_token_key
 // var access_token_secret = twitterKeys.access_token_secret
 
-console.log(twitterKeys.consumer_key)
+// console.log(twitterKeys.consumer_key)
 // console.log(consumer_key)
 
 var command = process.argv[2]
@@ -46,7 +47,41 @@ switch(command) {
 			})
 		}
 	case 'movie-this':
-		// do something different
+		// handle for no movie input
+		if (userData !== undefined){
+			var input = userData
+		} else {
+			var input = defaultMovie
+		}
+
+		var arr = input.trim().split(' ')
+		var format = ''
+		for (var i in arr) {
+			format = format + arr[i] + '+'
+		}
+		var final = format.substr(0, format.length-1)
+		var title = 't=' + final
+
+		request('http://www.omdbapi.com/?' + title + '&plot=short&tomatoes=true&r=json', function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				// console.log(JSON.parse(body)) // Show the HTML for the Google homepage.
+				var movieObj = JSON.parse(body)
+				console.log(movieObj.Title)
+				console.log(movieObj.Year)
+				console.log(movieObj.imdbRating)
+				console.log(movieObj.Country)
+				console.log(movieObj.Language)
+				console.log(movieObj.Plot)
+				console.log(movieObj.Actors)
+				console.log(movieObj.tomatoRating)
+				console.log(movieObj.tomatoURL)
+			} else {
+				console.warn(error);
+			}
+		});
+
+		// suppress the direct output of the call. you can expand the result below
+		"loading..."
 	case 'do-what-it-says':
 		// yet another thing 
 }
