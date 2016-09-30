8ec138369753cb9a7dc29c71f96b467c24f40ebf
MTibby92 <MTibby92@gmail.com>
2016-09-29 19:52:49 -0500

    initial commit with necessary files and templated data; need to resolve twitter account issue

---
+++ liri.js
@@ -0,0 +1,10 @@
+var twitterObj = require('./keys.js')
+var twitterKeys = twitterObj.twitterKeys
+
+// var consumer_key = twitterKeys.consumer_key
+// var consumer_secret = twitterKeys.consumer_secret
+// var access_token_key = twitterKeys.access_token_key
+// var access_token_secret = twitterKeys.access_token_secret
+
+console.log(twitterKeys.consumer_key)
+console.log(consumer_key)
