0c4e10287ddc3664c772740cad81c4a6ed00802a
MTibby92 <MTibby92@gmail.com>
2016-10-03 17:30:01 -0500

    added log functionality for tweets

--- liri.js
+++ liri.js
@@ -57,12 +57,27 @@ switch(command) {
 
 
 function myTweets() {
-	client.get('search/tweets', {q: '@mtibby92', count: '3'}, function(err, tweets, response) {
+	client.get('search/tweets', {q: '@SportsCenter', count: '20'}, function(err, tweets, response) {
 		if (err) {
 			return console.log(err)
 		} else {
-			console.log(tweets)
-			// console.log(response)
+			// console.log(tweets)
+			for (var i in tweets.statuses) {
+				var num = parseInt(i) + 1
+				console.log('Tweet #' + num)
+				console.log(tweets.statuses[i].text)
+				console.log(tweets.statuses[i].created_at)
+				console.log('========')
+
+				var str = 'Tweet #' + num + '\n' + tweets.statuses[i].text + '\n' + tweets.statuses[i].created_at + '\n========\n'
+				fs.appendFileSync('log.txt', str, 'utf8')
+			}
+			fs.appendFile('log.txt', '\n*****************************************\n\n', 'utf8', function(err) {
+				if (err) {
+					return console.log(err)
+				}
+				console.log('Log Updated!')
+			})
 		}
 	})
 }
